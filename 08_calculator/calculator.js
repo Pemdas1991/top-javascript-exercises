188eda913481d2a7a767f86e18ad030ea2056728
Pemdas1991 <Pemdas1991@gmail.com>
2022-08-03 15:56:01 -0600

    flesh out calculator project

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
+const initialValue = 0;
+
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(input) {
+  array = [...input]
+  let total = 0
+  array.forEach(num => {
+    total += num
+  });
+  return total;
+};
+
+//Sum can also be done as
+  // const sum = function(array) {
+  // return array.reduce((total, current) => total + current, 0);
+  // };
+
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1 )
+
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0){
+    return 1;
+  }
+  let output = 1;
+  for (i=1; i<=num; i++){
+    output *= i;
+  }
+  return output;
+  
 };
 
 // Do not edit below this line
