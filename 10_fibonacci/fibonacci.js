f5b140eb52ab8b3efa56faaa6ffb3f6487914c46
Pemdas1991 <Pemdas1991@gmail.com>
2022-08-03 16:41:25 -0600

    add fibonacci sequence exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num <0 ){
+        return "OOPS";
+    }
+    lastNumber = 0
+    nextNumber = 1
+    newNumber = 0
+    for (i=1; i<num; ++i){
+        newNumber = lastNumber + nextNumber;
+        lastNumber = nextNumber;
+        nextNumber = newNumber;
+    }
+    return nextNumber;
 };
 
 // Do not edit below this line
