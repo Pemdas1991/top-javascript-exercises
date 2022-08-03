12666ae4abab021613108a333dd38ef6ef049902
Pemdas1991 <Pemdas1991@gmail.com>
2022-08-03 16:23:56 -0600

    implement Palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let stringArray = [...string.toLowerCase()];
+    let newString = stringArray.reverse().toString().replace(/[^a-zA-Z0-9]/g, "");;
+    return newString === string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "") ? true : false
 };
 
 // Do not edit below this line
