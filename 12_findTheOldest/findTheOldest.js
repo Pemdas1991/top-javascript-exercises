eda5c50023377c4874e660e3ed44c2f55a5fffe7
Pemdas1991 <Pemdas1991@gmail.com>
2022-08-03 17:13:08 -0600

    implement find the oldest person in array of birth/death dates

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+
+const findTheOldest = function(people) {
+    oldest = people.reduce((lastPerson, nextPerson) => {
+        if (!nextPerson.yearOfDeath){
+            nextPerson.yearOfDeath = new Date().getFullYear()
+        } 
+        if (!lastPerson) {
+            return nextPerson;
+        } 
+        let lastAge = lastPerson.yearOfDeath - lastPerson.yearOfBirth;
+        let nextAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+
+        return lastAge > nextAge ? lastPerson : nextPerson;
+
+    }, 0)
+
+    return oldest;
 
 };
 
